2355b3e54ea081159785b71114bc966d6a43efb5
sintet92 <mayrog87@gmail.com>
2016-01-07 13:55:46 +0300

    asdasd

--- main.lua
+++ main.lua
@@ -1,22 +1,58 @@
+love.graphics.setDefaultFilter('nearest','nearest')
+enemy = {}
+enemies_controller = {}
+enemies_controller.enemies = {}
+enemies_controller.image= love.graphics.newImage('enemy.png')
+
 function love.load ()
+  -- player
   player = {}
   player.x = 0
-  player.y = 550
+  player.y = 500
   player.bullets = {}
   player.speed = 10
-  player.countdown = 20
+  player.cooldown = 0
+  player.image= love.graphics.newImage('player.png')
+  player.fire_sound = love.audio.newSource('SHOOT019.mp3')
   player.fire = function ()
-    if  player.countdown <= 0 then
-      player.countdown = 20
+    if  player.cooldown <= 0 then
+      love.audio.play(player.fire_sound)
+      player.cooldown = 60
       bullet = {}
-      bullet.x = player.x + 35
+      bullet.x = player.x + 25
       bullet.y = player.y;
       table.insert(player.bullets,bullet)
+
     end
   end
+  enemies_controller:spawnEnemy(0,0)
+  enemies_controller:spawnEnemy(90,0)
+  enemies_controller:spawnEnemy(180,0)
+
 end
+
+function enemies_controller:spawnEnemy (x, y)
+  enemy = {}
+  enemy.x = x
+  enemy.y = y
+  enemy.bullets = {}
+  enemy.speed = 10
+  enemy.cooldown = 20
+  table.insert(self.enemies,enemy)
+end
+
+function enemy:fire ()
+  if self.cooldown <=0 then
+    self.cooldown = 20
+    bullet = {}
+    bullet.x = self.x + 35
+    bullet.y = self.y;
+    table.insert(self.bullets,bullet)
+  end
+end
+
 function love.update (dt)
-  player.countdown = player.countdown - 1
+  player.cooldown = player.cooldown - 1
   if love.keyboard.isDown("d") then
     player.x =player.x + player.speed
   elseif love.keyboard.isDown("a") then
@@ -25,7 +61,11 @@ function love.update (dt)
   if love.keyboard.isDown(" ") then
       player.fire()
   end
-  for i,b in pairs(player.bullets) do
+  for _,e in ipairs(enemies_controller.enemies) do
+    e.y = e.y + 3
+  end
+
+  for i,b in ipairs(player.bullets) do
     if b.y < -10  then
       table.remove(player.bullets, i)
     end
@@ -33,12 +73,20 @@ function love.update (dt)
   end
 end
 function love.draw ()
+  -- love.graphics.scale(5)
   -- rect
  love.graphics.setColor(169, 199, 100)
- love.graphics.rectangle('fill',player.x, player.y, 80 , 40);
+ love.graphics.draw(player.image,player.x, player.y, 0 , 5, 5);
+ -- enemy
+ love.graphics.setColor(255, 255, 255)
+
+ for _,e in ipairs(enemies_controller.enemies) do
+   love.graphics.draw(enemies_controller.image, e.x,e.y, 0 , 5 ,5)
+ end
+
  -- bull
  love.graphics.setColor(255, 255, 255)
- for _,b in pairs(player.bullets) do
-   love.graphics.rectangle("fill", b.x, b.y, 10, 10)
+ for _,b in ipairs(player.bullets) do
+   love.graphics.rectangle("fill", b.x, b.y, 5, 5)
  end
 end
